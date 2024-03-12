45626fe89c1d425914443b7530733da9490a1363
Aman-Sirmaur19 <amansirmaur190402@gmail.com>
2024-03-12 17:18:10 +0530

    Implemented search feature.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_glow/flutter_glow.dart';
@@ -24,6 +25,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, dynamic>> pdfData = [];
 
+  // for storing search status
+  bool _isSearching = false;
+
+  //for storing searched items
+  final List<Map<String, dynamic>> _searchList = [];
+
   // for accessing files
   void getAllPdfs() async {
     final results = await APIs.storage.ref('PDFs').listAll();
@@ -45,50 +52,99 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> _refresh() async {
-    // didChangeDependencies();
     getAllPdfs();
     log(pdfData.length.toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PrepNight'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const GlowIcon(Icons.logout, color: Colors.redAccent),
-            onPressed: showAlertDialog,
-          )
-        ],
-      ),
-      drawer: MainDrawer(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.upload_file_outlined),
-        onPressed: () async {
-          final pickedFile = await FilePicker.platform.pickFiles(
-            type: FileType.custom,
-            allowedExtensions: ['pdf'],
-            allowCompression: true,
-          );
-          if (pickedFile != null) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => UploadPdfScreen(
-                  pickedFile: pickedFile,
-                  name: pickedFile.files[0].name,
-                  path: pickedFile.files[0].path!,
-                ),
-              ),
-            );
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: WillPopScope(
+        onWillPop: () {
+          if (_isSearching) {
+            setState(() {
+              _isSearching = !_isSearching;
+            });
+            return Future.value(false);
+          } else {
+            return Future.value(true);
           }
         },
-      ),
-      body: RefreshIndicator(
-        onRefresh: () => _refresh(),
-        child: pdfData.isEmpty ? _pdfDataIsEmpty() : _pdfDataIsNotEmpty(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: _isSearching
+                ? TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter PDF name...',
+                    ),
+                    autofocus: true,
+                    style: const TextStyle(fontSize: 17, letterSpacing: 1),
+
+                    // when search text changes, update search list
+                    onChanged: (val) {
+                      // search logic
+                      _searchList.clear();
+                      for (var i in pdfData) {
+                        if (i['name']
+                            .toLowerCase()
+                            .contains(val.toLowerCase())) {
+                          _searchList.add(i);
+                        }
+                        setState(() {
+                          _searchList;
+                        });
+                      }
+                    },
+                  )
+                : Text('PrepNight'),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: Icon(_isSearching
+                    ? CupertinoIcons.clear_circled_solid
+                    : CupertinoIcons.search),
+                onPressed: () {
+                  setState(() {
+                    _isSearching = !_isSearching;
+                  });
+                },
+              ),
+              IconButton(
+                icon: const GlowIcon(Icons.logout, color: Colors.redAccent),
+                onPressed: showAlertDialog,
+              )
+            ],
+          ),
+          drawer: MainDrawer(),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.upload_file_outlined),
+            onPressed: () async {
+              final pickedFile = await FilePicker.platform.pickFiles(
+                type: FileType.custom,
+                allowedExtensions: ['pdf'],
+                allowCompression: true,
+              );
+              if (pickedFile != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => UploadPdfScreen(
+                      pickedFile: pickedFile,
+                      name: pickedFile.files[0].name,
+                      path: pickedFile.files[0].path!,
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+          body: RefreshIndicator(
+            onRefresh: () => _refresh(),
+            child: pdfData.isEmpty ? _pdfDataIsEmpty() : _pdfDataIsNotEmpty(),
+          ),
+        ),
       ),
     );
   }
@@ -148,7 +204,7 @@ class _HomeScreenState extends State<HomeScreen> {
         crossAxisSpacing: 8.0,
         mainAxisSpacing: 8.0,
       ),
-      itemCount: pdfData.length,
+      itemCount: _isSearching ? _searchList.length : pdfData.length,
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.all(8.0),
@@ -156,14 +212,22 @@ class _HomeScreenState extends State<HomeScreen> {
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => PdfViewerScreen(
-                        pdfName: pdfData[index]['name'],
-                        pdfUrl: pdfData[index]['url'],
+                        pdfName: _isSearching
+                            ? _searchList[index]['name']
+                            : pdfData[index]['name'],
+                        pdfUrl: _isSearching
+                            ? _searchList[index]['url']
+                            : pdfData[index]['url'],
                       )));
             },
             child: Column(
               children: [
                 Image.asset('assets/images/pdf.png', width: mq.width * .35),
-                Text(pdfData[index]['name'], maxLines: 1),
+                Text(
+                    _isSearching
+                        ? _searchList[index]['name']
+                        : pdfData[index]['name'],
+                    maxLines: 1),
               ],
             ),
           ),
@@ -172,27 +236,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-// // for accessing files
-// void getAllPdfs() async {
-//   final results = await APIs.firestore.collection('PDFs').get();
-//   pdfData = results.docs.map((e) => e.data()).toList();
-//   setState(() {});
-// }
-//
-// @override
-// void initState() {
-//   super.initState();
-//   getAllPdfs();
-// }
-//
-// @override
-// void didChangeDependencies() {
-//   super.didChangeDependencies();
-//   getAllPdfs();
-//   log(pdfData.length.toString());
-// }
-//
-// Future<void> _refresh() async {
-//   didChangeDependencies();
-// }
